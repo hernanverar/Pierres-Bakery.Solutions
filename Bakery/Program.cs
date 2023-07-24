036718c9a2964c45774061672211c2d7e717150b
hernan verar <hernanverar@gmail.com>
2023-07-23 17:40:14 -0700

    add upgrades to our Begin Order code and upgraded and added confirm order to our Bread and Pastry Orders

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -6,6 +6,8 @@ class Program
 {
   static void Main()
   {
+    Console.Clear();
+    Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
     Console.WriteLine("Welcome to Pierres Bakery best Bread and Pastry's in Town");
     Console.WriteLine("What whould you like to order");
     Console.WriteLine("Bread Prices: one loaf of bread cost $5 dollars");
@@ -13,20 +15,19 @@ class Program
     Console.WriteLine("Pastry Prices: Pastry cost $2 dollars");
     Console.WriteLine("Pastry Deals: 3 pastry's for $5dls, 4 pastry's for $7dls, 5 pastry's for $9dls, 6 pastry's for $10dls or 12 pastry's for $18dls");
     Console.WriteLine("How Many loafs of Bread would you like to purchase?");
-    Console.WriteLine("Welcome to Pierre's Bakery!");
     Console.WriteLine("Whould you like to place an order?");
     Console.WriteLine("Yes or No?");
-    string stringPlaceOrder = Console.ReadLine();
-      if (stringPlaceOrder == "yes" || stringPlaceOrder == "Yes")
+    string stringBeginOrder = Console.ReadLine();
+      if (stringBeginOrder == "yes" || stringBeginOrder == "Yes" || stringBeginOrder == "y" || stringBeginOrder == "Y")
       {
-        PlaceOrder();
+        BeginOrder();
       } 
     else 
       {
         Console.WriteLine("Come back soon!");
       }
   }
-  static void PlaceOrder()
+  static void BeginOrder()
   {
     Console.Clear();
     Console.WriteLine("What would you like to order?");
@@ -42,56 +43,49 @@ class Program
       }
     else 
       {
-        Console.WriteLine("Please make a choice! 'Bread' or 'Pastry'");
-        PlaceOrder();
+        Console.WriteLine("Please type either 'Bread' or 'Pastry'");
+        BeginOrder();
       }
   }
-    static void PlaceBreadOrder()
+  
+  static void PlaceBreadOrder()
   {
-    Console.WriteLine("Welcome, loaves are $5 or buy 2 get one free!");
+    Console.WriteLine("Awesome, loaves are $5 or buy 2 get one free!");
     Console.WriteLine("How many loaves of bread would you like to order?");
     string stringBreadOrderQty = Console.ReadLine();
     int breadOrderQty = int.Parse(stringBreadOrderQty);
     Bread breadOrder = new Bread(breadOrderQty);
-    Console.WriteLine("Order Placed!");
+    ConfirmOrEditBreadOrder(breadOrder);
   }
-    static void PlacePastryOrder()
-  {
-    Console.WriteLine("Welcome, pastries are $2 or buy 3 get one free!");
-    Console.WriteLine("How many pastries would you like to order?");
-    string stringPastryOrderQty = Console.ReadLine();
-    int PastryOrderQty = int.Parse(stringPastryOrderQty);
-    Pastry pastryOrder = new Pastry(PastryOrderQty);
-    Console.WriteLine("Order Placed!");
-  }
+
   static void ConfirmOrEditBreadOrder(Bread breadOrder)
   {
-    Console.WriteLine("Is your order correct?");
+    Console.WriteLine("Does your order look correct?");
     Console.WriteLine($"Order: {breadOrder.BreadQty} loaves of bread for ${breadOrder.BreadOrderPrice}.");
     Console.WriteLine("Is that correct? Enter 'yes' to proceed, or 'no' to re-enter your bread order");
     string breadOrderCheckInput = Console.ReadLine();
-    if (breadOrderCheckInput == "yes" || breadOrderCheckInput == "YES")
+    if (breadOrderCheckInput == "yes" || breadOrderCheckInput == "YES" || breadOrderCheckInput == "Yes" || breadOrderCheckInput == "y" || breadOrderCheckInput == "Y")
     {
       Console.WriteLine("Order Confirmed!");
-      Console.WriteLine("Would you TO buy  some pastries too?");
+      Console.WriteLine("Would you like to order some pastries too?");
       string addPastryToBreadOrder = Console.ReadLine();
-      if (addPastryToBreadOrder == "yes" || addPastryToBreadOrder == "YES")
+      if (addPastryToBreadOrder == "yes" || addPastryToBreadOrder == "YES" || addPastryToBreadOrder == "Yes" || addPastryToBreadOrder == "y" || addPastryToBreadOrder == "Y")
       {
         PlacePastryOrder();
       }
       else
       {
         Console.WriteLine("Order Placed!");
-        Console.WriteLine("Your Order will be ready shortly");
-        Console.WriteLine("Thank you, comeback soon!");
+        Console.WriteLine("It will be ready shortly");
+        Console.WriteLine("Thank you and have a great day!");
       }
     }
     else 
     {
       PlaceBreadOrder();
     }
-
-      static void PlacePastryOrder()
+  }
+    static void PlacePastryOrder()
   {
     Console.WriteLine("Great, pastries are $2 or buy 3 get one free!");
     Console.WriteLine("How many pastries would you like to order?");
@@ -102,30 +96,29 @@ class Program
   }
 static void ConfirmOrEditPastryOrder(Pastry pastryOrder)
   {
-    Console.WriteLine("Does your order look Good?");
+    Console.WriteLine("Does your order look correct?");
     Console.WriteLine($"Order: {pastryOrder.PastryQty} pastries for ${pastryOrder.PastryOrderPrice}.");
     Console.WriteLine("Is that correct? Enter 'yes' to proceed, or 'no' to re-enter your pastry order");
     string pastryOrderCheckInput = Console.ReadLine();
-    if (pastryOrderCheckInput == "yes" || pastryOrderCheckInput == "YES")
+    if (pastryOrderCheckInput == "yes" || pastryOrderCheckInput == "YES" || pastryOrderCheckInput == "Yes" || pastryOrderCheckInput == "y" || pastryOrderCheckInput == "Y")
     {
       Console.WriteLine("Order Confirmed!");
-      Console.WriteLine("Would you like to place an order of bread too?");
+      Console.WriteLine("Would you like to order some bread too?");
       string addBreadToPastryOrder = Console.ReadLine();
-      if (addBreadToPastryOrder == "yes" || addBreadToPastryOrder == "YES")
+      if (addBreadToPastryOrder == "yes" || addBreadToPastryOrder == "YES" || addBreadToPastryOrder == "Yes" || addBreadToPastryOrder == "y" || addBreadToPastryOrder == "Y")
       {
         PlaceBreadOrder();
       }
       else
       {
         Console.WriteLine("Order Placed!");
-        Console.WriteLine("Your order will be ready shortly");
-        Console.WriteLine("Thank you and come back  soon!");
+        Console.WriteLine("It will be ready shortly");
+        Console.WriteLine("Thank you and have a great day!");
       }
     }
     else 
     {
       PlacePastryOrder();
     }
-  }
   }
 }
